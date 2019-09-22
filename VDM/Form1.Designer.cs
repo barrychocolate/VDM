9e9300ba6a53408aa8c29243d8677015c5f5f18f
Barry Chocolate <barrychocolate@yahoo.co.uk>
2019-09-22 23:53:52 +0100

    Add files via upload

---
+++ VDM/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace VDM
+{
+    partial class frmVDM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVDM));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.tvwLinks = new System.Windows.Forms.TreeView();
+            this.btnTreeview = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status_label = new System.Windows.Forms.ToolStripStatusLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsNotifyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topLeftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topRightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bottomRightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bottomLeftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnMoveRight = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.cmsNotifyIcon.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(255, 27);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(174, 27);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(93, 27);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnMessage.TabIndex = 1;
+            this.btnMessage.Text = "Message";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.BtnMessage_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 27);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.BtnCreate_Click);
+            // 
+            // tvwLinks
+            // 
+            this.tvwLinks.Location = new System.Drawing.Point(12, 85);
+            this.tvwLinks.Name = "tvwLinks";
+            this.tvwLinks.Size = new System.Drawing.Size(324, 484);
+            this.tvwLinks.TabIndex = 4;
+            this.tvwLinks.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TvwLinks_AfterSelect);
+            // 
+            // btnTreeview
+            // 
+            this.btnTreeview.Location = new System.Drawing.Point(12, 56);
+            this.btnTreeview.Name = "btnTreeview";
+            this.btnTreeview.Size = new System.Drawing.Size(75, 23);
+            this.btnTreeview.TabIndex = 5;
+            this.btnTreeview.Text = "build treeview";
+            this.btnTreeview.UseVisualStyleBackColor = true;
+            this.btnTreeview.Click += new System.EventHandler(this.BtnTreeview_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status_label});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 559);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(348, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status_label
+            // 
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(0, 17);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.cmsNotifyIcon;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "VDM";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.NotifyIcon1_Click);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // cmsNotifyIcon
+            // 
+            this.cmsNotifyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.cmsNotifyIcon.Name = "cmsNotifyIcon";
+            this.cmsNotifyIcon.Size = new System.Drawing.Size(104, 48);
+            this.cmsNotifyIcon.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip_Opening);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(94, 56);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(75, 23);
+            this.btnMin.TabIndex = 7;
+            this.btnMin.Text = "Minimize";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.BtnMin_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(348, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dockToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.FileToolStripMenuItem_Click);
+            // 
+            // dockToolStripMenuItem
+            // 
+            this.dockToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topLeftToolStripMenuItem,
+            this.topRightToolStripMenuItem,
+            this.bottomRightToolStripMenuItem,
+            this.bottomLeftToolStripMenuItem});
+            this.dockToolStripMenuItem.Name = "dockToolStripMenuItem";
+            this.dockToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dockToolStripMenuItem.Text = "Dock";
+            // 
+            // topLeftToolStripMenuItem
+            // 
+            this.topLeftToolStripMenuItem.Name = "topLeftToolStripMenuItem";
+            this.topLeftToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.topLeftToolStripMenuItem.Text = "Top Left";
+            this.topLeftToolStripMenuItem.Click += new System.EventHandler(this.TopLeftToolStripMenuItem_Click);
+            // 
+            // topRightToolStripMenuItem
+            // 
+            this.topRightToolStripMenuItem.Name = "topRightToolStripMenuItem";
+            this.topRightToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.topRightToolStripMenuItem.Text = "Top Right";
+            this.topRightToolStripMenuItem.Click += new System.EventHandler(this.TopRightToolStripMenuItem_Click);
+            // 
+            // bottomRightToolStripMenuItem
+            // 
+            this.bottomRightToolStripMenuItem.Checked = true;
+            this.bottomRightToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bottomRightToolStripMenuItem.Name = "bottomRightToolStripMenuItem";
+            this.bottomRightToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bottomRightToolStripMenuItem.Text = "Bottom Right";
+            this.bottomRightToolStripMenuItem.Click += new System.EventHandler(this.BottomRightToolStripMenuItem_Click);
+            // 
+            // bottomLeftToolStripMenuItem
+            // 
+            this.bottomLeftToolStripMenuItem.Name = "bottomLeftToolStripMenuItem";
+            this.bottomLeftToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bottomLeftToolStripMenuItem.Text = "Bottom Left";
+            this.bottomLeftToolStripMenuItem.Click += new System.EventHandler(this.BottomLeftToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.ExitToolStripMenuItem1_Click);
+            // 
+            // btnMoveRight
+            // 
+            this.btnMoveRight.Location = new System.Drawing.Point(175, 56);
+            this.btnMoveRight.Name = "btnMoveRight";
+            this.btnMoveRight.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveRight.TabIndex = 9;
+            this.btnMoveRight.Text = "move right";
+            this.btnMoveRight.UseVisualStyleBackColor = true;
+            this.btnMoveRight.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // frmVDM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 581);
+            this.Controls.Add(this.btnMoveRight);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnTreeview);
+            this.Controls.Add(this.tvwLinks);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnMessage);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmVDM";
+            this.Text = "VDM";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmVDM_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.cmsNotifyIcon.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TreeView tvwLinks;
+        private System.Windows.Forms.Button btnTreeview;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status_label;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.ContextMenuStrip cmsNotifyIcon;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem topRightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topLeftToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bottomRightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bottomLeftToolStripMenuItem;
+        private System.Windows.Forms.Button btnMoveRight;
+    }
+}
+
---
+++ VDM/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace VDM
+{
+    public partial class frmVDM : Form
+    {
+        enum DockPostion { TopLeft = 0, TopRight, BottomRight, BottomLeft};
+
+        private string xml_path = @"C:\test\vdm.xml";
+        private DataTable vdmData = null;
+
+        public frmVDM()
+        {
+            InitializeComponent();
+        }
+
+        private void create_datatable()
+        {
+            vdmData = new DataTable { TableName = "VDM_Data" };
+
+            DataColumn column = new DataColumn("id", System.Type.GetType("System.Int32"));
+            vdmData.Columns.Add(column);
+
+            column = new DataColumn("parent_id", System.Type.GetType("System.Int32"));
+            column.AllowDBNull = true;
+            vdmData.Columns.Add(column);
+
+            column = new DataColumn("link_type", System.Type.GetType("System.Int32"));
+            vdmData.Columns.Add(column);
+
+            column = new DataColumn("name", System.Type.GetType("System.String"));
+            vdmData.Columns.Add(column);
+
+            column = new DataColumn("url", System.Type.GetType("System.String"));
+            vdmData.Columns.Add(column);
+
+
+            DataRow new_row = vdmData.NewRow();
+
+            new_row["id"] = 1;
+            new_row["parent_id"] = DBNull.Value;
+            new_row["link_type"] = 1;
+            new_row["name"] = "Root 1";
+            new_row["url"] = "root 1 url";
+            vdmData.Rows.Add(new_row);
+
+            new_row = vdmData.NewRow();
+            new_row["id"] = 2;
+            new_row["parent_id"] = 1;
+            new_row["link_type"] = 1;
+            new_row["name"] = "Root 1 sub 1";
+            new_row["url"] = "root 1 sub 1 url";
+            vdmData.Rows.Add(new_row);
+
+            new_row = vdmData.NewRow();
+            new_row["id"] = 3;
+            new_row["parent_id"] = DBNull.Value;
+            new_row["link_type"] = 1;
+            new_row["name"] = "Root 2";
+            new_row["url"] = "root 2 url";
+            vdmData.Rows.Add(new_row);
+        }
+        private void save_XML()
+        {
+           vdmData.WriteXml(xml_path, System.Data.XmlWriteMode.WriteSchema);
+            status_label.Text = string.Format("DataTable saved");
+        }
+        private void load_XML()
+        {
+            vdmData = new DataTable();
+            vdmData.ReadXml(xml_path);
+            if (vdmData != null)
+            {
+                status_label.Text = string.Format("Data loaded from file");
+            }
+            else
+            {
+                status_label.Text = string.Format("Data did not load from file");
+            }
+        }
+
+        private void PopulateTree()
+        {
+
+            TreeNode node = tvwLinks.Nodes.Add("1", "test");
+            node.Nodes.Add("2", "Node 1 Sub 1");
+            tvwLinks.Nodes.Add("3","Node 2");
+        }
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            create_datatable();
+            save_XML();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            load_XML();
+        }
+
+        private void BtnMessage_Click(object sender, EventArgs e)
+        {
+            if (vdmData != null)
+            {
+                foreach (DataRow row in vdmData.Rows)
+                {
+                    // ... Write value of first field as integer.
+                    status_label.Text = string.Format(row["id"].ToString());
+                    //string.Format(row.Field<string>(0));
+
+                }
+
+                MessageBox.Show("all rows looped through");
+            }
+            else
+            {
+                MessageBox.Show("There is no data loaded");
+            }
+        }
+
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            create_datatable();
+        }
+
+        private void BtnTreeview_Click(object sender, EventArgs e)
+        {
+            PopulateTree();
+        }
+
+        private void TvwLinks_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // Get the selected node
+            TreeNode node = tvwLinks.SelectedNode;
+            
+            //Check node has a name
+            if (node.Name=="")
+            {
+                // Root node so do nothing 
+                status_label.Text = string.Format("root node so do nothing");
+                
+            }
+            else
+            {
+                // Create query string using the id of the selected node
+                string select_query = string.Format("id = {0}", node.Name.ToString());
+
+                // Query the datatables
+                DataRow[] rows = vdmData.Select(select_query);
+
+                // Deal with the URL
+                //MessageBox.Show(string.Format("Your selected url: {0}", rows[0][4].ToString()));
+                status_label.Text = string.Format("Your selected url: {0}", rows[0][4].ToString());
+            }
+
+        }
+
+        private void BtnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "Notify Icon Test Application";
+            notifyIcon1.BalloonTipText = "You have just minimized the application." +
+                                        Environment.NewLine +
+                                        "Right-click on the icon for more options.";
+
+            notifyIcon1.ShowBalloonTip(5000);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get prefared docking location from settings
+            //TODO add code to get position
+            DockPostion prefaredPosition = DockPostion.BottomRight;
+
+            //Set the position
+            DockTheForm(prefaredPosition);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //todo Add form closing action
+            //Save Dock location to settings
+        }
+
+        private void ContextMenuStrip_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void NotifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+        private void ExitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void DockTheForm(DockPostion position)
+        {
+            //Dock the form using the supplied screen position
+            Console.WriteLine(string.Format("Docking the form {0}" , position.ToString()));
+
+            //Set x and y to zero which is the default for top left docking
+            int x = 0;
+            int y = 0;
+
+            if (position == DockPostion.BottomRight ^ position == DockPostion.TopRight)
+            {
+                x = (int)(System.Windows.SystemParameters.WorkArea.Width - this.Width);
+            }
+
+            if (position == DockPostion.BottomLeft ^ position==DockPostion.BottomRight)
+            {
+                y = (int)(System.Windows.SystemParameters.WorkArea.Height - this.Height);
+            }
+            //Now position form
+            this.DesktopLocation = new Point(x, y);
+
+            //Check the menu item for the docking position
+            switch (position)
+            {
+                case DockPostion.TopLeft:
+                    UncheckOtherToolStripMenuItems(topLeftToolStripMenuItem);
+                    break;
+                case DockPostion.TopRight:
+                    UncheckOtherToolStripMenuItems(topRightToolStripMenuItem);
+                    break;
+                case DockPostion.BottomRight:
+                    UncheckOtherToolStripMenuItems(bottomRightToolStripMenuItem);
+                    break;
+                case DockPostion.BottomLeft:
+                    UncheckOtherToolStripMenuItems(bottomLeftToolStripMenuItem);
+                    break;
+            }
+        }
+
+        public void UncheckOtherToolStripMenuItems(ToolStripMenuItem selectedMenuItem)
+        {
+            selectedMenuItem.Checked = true;
+
+            // Select the other MenuItens from the ParentMenu(OwnerItens) and unchecked this,
+            // The current Linq Expression verify if the item is a real ToolStripMenuItem
+            // and if the item is a another ToolStripMenuItem to uncheck this.
+            foreach (var ltoolStripMenuItem in (from object
+                                                    item in selectedMenuItem.Owner.Items
+                                                let ltoolStripMenuItem = item as ToolStripMenuItem
+                                                where ltoolStripMenuItem != null
+                                                where !item.Equals(selectedMenuItem)
+                                                select ltoolStripMenuItem))
+                (ltoolStripMenuItem).Checked = false;
+        }
+
+        private void BottomRightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DockTheForm(DockPostion.BottomRight);
+        }
+        private void TopLeftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DockTheForm(DockPostion.TopLeft);
+        }
+        private void TopRightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DockTheForm(DockPostion.TopRight);
+        }
+        private void BottomLeftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DockTheForm(DockPostion.BottomLeft);
+        }
+
+        private void FileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //this.Location = new Point(0, 0);
+            int x = this.Left;
+            int y = this.Top;
+
+            x = x + 100;
+            this.Location = new Point(x, y);
+            Console.WriteLine(string.Format("The x is {0} and the y is {1}", x, y));
+        }
+
+        private void FrmVDM_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
